06274c130ea7e4161ed8ba395874550b6758f0c6
QabylzhaparovAlinur <alinurkabylzhaparov@gmail.com>
2024-02-19 19:24:29 +0600

    no

--- lab4/angular-main/src/app/app.module.ts
+++ lab4/angular-main/src/app/app.module.ts
@@ -3,12 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
+=======
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+>>>>>>> parent of 2f7c597 (4)
   declarations: [
     AppComponent,
     MainLayoutComponent,
