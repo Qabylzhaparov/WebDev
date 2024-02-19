2f7c597e3910135c97992930c35c9a48fe65b87a
QabylzhaparovAlinur <alinurkabylzhaparov@gmail.com>
2024-02-19 18:56:56 +0600

    4

--- lab4/angular-main/src/app/app.module.ts
+++ lab4/angular-main/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import {ProductAlertsComponent} from "./product-alerts/product-alerts.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+      {path: '', component: ProductListComponent},
+    ]),
+    ProductAlertsComponent
   ],
   declarations: [
     AppComponent,
