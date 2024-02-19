57161a35a1588cb7b5fa76c02e9213e274b433bd
QabylzhaparovAlinur <alinurkabylzhaparov@gmail.com>
2024-02-20 01:48:54 +0600

    final

--- lab4/online-store/src/app/product-list/product-list.component.ts
+++ lab4/online-store/src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { Product} from "../products";
 
 @Component({
   selector: 'app-product-list',
@@ -8,15 +9,16 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = [...products];
+  products: Product[] = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  open(product: Product) {
+    window.open(product.link);
   }
 
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale')
+  share(product: Product){
+    window.open(`https://t.me/share/url?url=${encodeURIComponent(product.link)}`);
   }
+
 }
 
 
